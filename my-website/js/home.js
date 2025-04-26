0abd9bdff65143c8afcb8651404beb1ed45e887f
Lhuckz <gen3custodian@gmail.com>
2025-04-27 00:15:38 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '9d6b6347d7a032511c5f916c68bddce2';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
